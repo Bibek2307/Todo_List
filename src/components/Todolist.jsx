210ed8ef943d182b3911e2106e0bdce7d30fc8eb
Bibek2307 <bibek.mukherjee2307@gmail.com>
2025-08-03 16:16:16 +0530

    final

--- src/components/Todolist.jsx
+++ src/components/Todolist.jsx
@@ -1,9 +1,40 @@
 import React from 'react'
-
+import { useState } from 'react'
+import "../style.css"
 const Todolist = () => {
+
+    const[todos,setTodos]= useState([])
+    const[input,setInput]=useState('')
+
+    const handlesubmit=()=>{
+        setTodos((todos)=>{
+           return todos.concat({
+            text: input,
+            id: Math.floor(Math.random() * 100)
+        })
+        })
+    
+        setInput("") 
+    }
+
+    const removeTodo=(id)=>{
+        setTodos(todos=> todos.filter(t=> t.id!=id))
+    }
+
   return (
-    <div>
-      
+    <div className='container'>
+        <input className='inputbox' type="text" value={input} placeholder='Enter your Todo' onChange={e=>setInput(e.target.value)} />
+        <button className='inputsubmit' onClick={handlesubmit}>
+            Submit
+        </button>
+        <ul className='todolist'>
+            {todos.map(({text,id})=>(
+                <li className='todoitem' key={id}>
+                <span className='txt' >{text}</span>
+                <button className='textbtn' onClick={()=>removeTodo(id)}>x</button>
+                </li>
+            ))}
+        </ul>
     </div>
   )
 }
